eae98b81390953fd5183bac0027599845fcef098
Alekslavl <1.alekslove@gmail.com>
2024-10-23 13:53:58 +0300

    made a change to login.page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,7 @@
 from .base_page import BasePage
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,8 +11,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert "login" in driver.current_url, "Incorrect url address"
+        # реализуйте проверку на корректный url адрес       
+        assert "login" in self.browser.current_url, "Incorrect url address"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
